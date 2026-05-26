7e5408d0e2bb1f733afec99f1d46717257a57c56
Lilian Solera <lilianmsolera@gmail.com>
2026-05-25 21:34:15 -0300

    adição do endpoint /pokemon/id para puxar types

--- bronze-layer/scripts/export.py
+++ bronze-layer/scripts/export.py
@@ -5,24 +5,30 @@ import requests
 
 try:
     for i in range(1, 152):
-        response_pokemon = requests.get(
+        # CHAMA ENDPOINTS
+        response_pokemon = requests.get(f"https://pokeapi.co/api/v2/pokemon/{i}")
+        response_pokemon_species = requests.get(
             f"https://pokeapi.co/api/v2/pokemon-species/{i}"
         )
         response_encounters = requests.get(
             f"https://pokeapi.co/api/v2/pokemon/{i}/encounters"
         )
 
+        # ARMAZENA EM JSON
         pokemon = response_pokemon.json()
+        pokemon_species = response_pokemon_species.json()
         encounters = response_encounters.json()
 
-        pokemon_name = pokemon["varieties"][0]["pokemon"]["name"]
+        pokemon_name = pokemon_species["varieties"][0]["pokemon"]["name"]
+
+        # CRIA ARQUIVOS
         with open(
             f"C:\\Users\\Remakker\\PDI-lilian\\pokemon-pdi\\bronze-layer\\pokemon-species\\{i}_{pokemon_name}.json",
             "w",
             encoding="utf-8",
         ) as arquivo_pokemon:
             json.dump(
-                pokemon, arquivo_pokemon, ensure_ascii=False, indent=4
+                pokemon_species, arquivo_pokemon, ensure_ascii=False, indent=4
             )  # ensure_ascii = false permite caracteres especiais
 
         with open(
@@ -31,6 +37,14 @@ try:
             encoding="utf-8",
         ) as arquivo_encounter:
             json.dump(encounters, arquivo_encounter, ensure_ascii=False, indent=4)
+
+        with open(
+            f"C:\\Users\\Remakker\\PDI-lilian\\pokemon-pdi\\bronze-layer\\pokemon\\{i}_{pokemon_name}.json",
+            "w",
+            encoding="utf-8",
+        ) as arquivo_poke:
+            json.dump(pokemon, arquivo_poke, ensure_ascii=False, indent=4)
+
         print(f"ID {i} armazenado com sucesso")
         time.sleep(2)
     print("Processamento concluído")
